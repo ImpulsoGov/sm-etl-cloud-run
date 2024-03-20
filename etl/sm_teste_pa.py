f8c7f167a8f8f2ab496d854adf7b9d4f7f8c857e
Alexsandro <alexsandro2134@gmail.com>
2024-03-20 10:26:35 -0300

    aumenta passo default do processo de extrair lotes dbc

--- etl/sm_teste_pa.py
+++ etl/sm_teste_pa.py
@@ -143,7 +143,7 @@ def extrair_dbc_lotes(
     ftp: str,
     caminho_diretorio: str,
     arquivo_nome: str | re.Pattern,
-    passo: int = 10000,
+    passo: int = 1000000,
     **kwargs,
 ) -> Generator[pd.DataFrame, None, None]:
     """Extrai dados de um arquivo .dbc do FTP do DataSUS e retorna DataFrames.
