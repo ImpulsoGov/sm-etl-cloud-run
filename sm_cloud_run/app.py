543fe6cde7451f27d5b7ba19fe1f98c97bf04e9a
fernandesbruna <brunadesfernandes@gmail.com>
2024-06-12 16:06:29 -0300

    feat: :heavy_plus_sign: Adiciona rota no arquivo app.py para script de loading no banco postgres

--- sm_cloud_run/app.py
+++ sm_cloud_run/app.py
@@ -4,6 +4,7 @@ import datetime
 from flask import Flask, request, jsonify
 
 from etl.procedimentos_ambulatoriais import baixar_e_processar_pa
+from load_bd.procedimentos_ambulatoriais_l_bd import inserir_pa_postgres
 
 app = Flask(__name__)
 
@@ -21,6 +22,19 @@ def sm_pa():
     return jsonify(baixar_e_processar_pa(json_params['UF'], data_datetime))
 
 
+@app.route("/pa_postgres", methods=['POST'])
+def load_pa():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json_params = request.json
+    else:
+        return 'Erro, content-type deve ser json', 400
+
+    data_datetime = datetime.datetime.strptime(json_params['data'], "%Y-%m-%d")
+
+    return jsonify(inserir_pa_postgres(json_params['UF'], data_datetime, json_params['tabela_destino']))
+
+
 # @app.route("/bpai", methods=['POST'])
 # def sm_bpai():
 #     content_type = request.headers.get('Content-Type')
@@ -32,6 +46,8 @@ def sm_pa():
 #     data_datetime = datetime.datetime.strptime(json_params['data'], "%Y-%m-%d")
 
 #     return jsonify(baixar_e_processar_bpai(json_params['UF'], data_datetime))
+
+
 
 
 if __name__ == "__main__":
