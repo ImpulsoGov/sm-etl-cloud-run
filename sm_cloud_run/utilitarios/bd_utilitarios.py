710c3c738ed10949554e335c3a68fc26f4908821
fernandesbruna <brunadesfernandes@gmail.com>
2024-06-12 16:08:05 -0300

    fix: :bulb: Remove linhas comentadas do arquivo que passaram a ser utilizadas a partir desse PR

--- sm_cloud_run/utilitarios/bd_utilitarios.py
+++ sm_cloud_run/utilitarios/bd_utilitarios.py
@@ -75,173 +75,173 @@ class TabelasRefletidasDicionario(dict):
             self[chave] = valor
 
 
-# def postgresql_copiar_dados(
-#     tabela_dados: SQLTable,
-#     conexao: Connection | Engine,
-#     colunas: Iterable[str],
-#     dados_iterador: Iterable,
-# ) -> None:
-#     """Inserir dados em uma tabela usando o comando COPY do PostgreSQL.
+def postgresql_copiar_dados(
+    tabela_dados: SQLTable,
+    conexao: Connection | Engine,
+    colunas: Iterable[str],
+    dados_iterador: Iterable,
+) -> None:
+    """Inserir dados em uma tabela usando o comando COPY do PostgreSQL.
 
-#     Esta função deve ser passada como valor para o argumento `method` do
-#     método [`pandas.DataFrame.to_sql()`][].
+    Esta função deve ser passada como valor para o argumento `method` do
+    método [`pandas.DataFrame.to_sql()`][].
 
-#     Argumentos:
-#         tabela_dados: objeto [`pandas.io.sql.SQLTable`][] com a representação
-#             da tabela SQL a ser inserida.
-#         conexao: objeto [`sqlalchemy.engine.Engine`][] ou
-#             [`sqlalchemy.engine.Connection`][] contendo a conexão de alto nível
-#             com o banco de dados gerenciada pelo SQLAlchemy.
-#         colunas: lista com os nomes de colunas a serem inseridas.
-#         dados_iterador: Iterável com os dados a serem inseridos.
+    Argumentos:
+        tabela_dados: objeto [`pandas.io.sql.SQLTable`][] com a representação
+            da tabela SQL a ser inserida.
+        conexao: objeto [`sqlalchemy.engine.Engine`][] ou
+            [`sqlalchemy.engine.Connection`][] contendo a conexão de alto nível
+            com o banco de dados gerenciada pelo SQLAlchemy.
+        colunas: lista com os nomes de colunas a serem inseridas.
+        dados_iterador: Iterável com os dados a serem inseridos.
 
-#     Exceções:
-#         Levanta uma subclasse da exceção [`psycopg2.Error`][] caso algum erro
-#         seja retornado pelo backend.
+    Exceções:
+        Levanta uma subclasse da exceção [`psycopg2.Error`][] caso algum erro
+        seja retornado pelo backend.
 
-#     Veja também:
-#         - [Documentação][io-sql-method] do Pandas sobre a implementação de
-#         funções personalizadas de inserção.
-#         - [Artigo][insert-a-pandas-dataframe-into-postgres] com comparação da
-#         performance de métodos de inserção de DataFrames bancos de dados
-#         PostgreSQL.
+    Veja também:
+        - [Documentação][io-sql-method] do Pandas sobre a implementação de
+        funções personalizadas de inserção.
+        - [Artigo][insert-a-pandas-dataframe-into-postgres] com comparação da
+        performance de métodos de inserção de DataFrames bancos de dados
+        PostgreSQL.
 
-#     [`pandas.DataFrame.to_sql()`]: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
-#     [`sqlalchemy.engine.Engine`]: https://docs.sqlalchemy.org/en/14/core/connections.html#sqlalchemy.engine.Engine
-#     [`sqlalchemy.engine.Connection`]: https://docs.sqlalchemy.org/en/14/core/connections.html#sqlalchemy.engine.Connection
-#     [`pandas.io.sql.SQLTable`]: https://github.com/pandas-dev/pandas/blob/a8968bfa696d51f73769c54f2630a9530488236a/pandas/io/sql.py#L762
-#     [`psycopg2.Error`]: https://www.psycopg.org/docs/module.html#psycopg2.Error
-#     [io-sql-method]: https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#io-sql-method
-#     [insert-a-pandas-dataframe-into-postgres]: https://ellisvalentiner.com/post/a-fast-method-to-insert-a-pandas-dataframe-into-postgres
-#     """
+    [`pandas.DataFrame.to_sql()`]: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
+    [`sqlalchemy.engine.Engine`]: https://docs.sqlalchemy.org/en/14/core/connections.html#sqlalchemy.engine.Engine
+    [`sqlalchemy.engine.Connection`]: https://docs.sqlalchemy.org/en/14/core/connections.html#sqlalchemy.engine.Connection
+    [`pandas.io.sql.SQLTable`]: https://github.com/pandas-dev/pandas/blob/a8968bfa696d51f73769c54f2630a9530488236a/pandas/io/sql.py#L762
+    [`psycopg2.Error`]: https://www.psycopg.org/docs/module.html#psycopg2.Error
+    [io-sql-method]: https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#io-sql-method
+    [insert-a-pandas-dataframe-into-postgres]: https://ellisvalentiner.com/post/a-fast-method-to-insert-a-pandas-dataframe-into-postgres
+    """
 
-#     try:
-#         # obter conexão de DBAPI a partir de uma conexão existente
-#         conector_dbapi = conexao.connection  # type: ignore
-#     except AttributeError:
-#         # obter conexão de DBAPI diretamente a partir da engine;
-#         # ver https://docs.sqlalchemy.org/en/14/core/connections.html
-#         # #working-with-the-dbapi-cursor-directly
-#         conector_dbapi = conexao.raw_connection()  # type: ignore
+    try:
+        # obter conexão de DBAPI a partir de uma conexão existente
+        conector_dbapi = conexao.connection  # type: ignore
+    except AttributeError:
+        # obter conexão de DBAPI diretamente a partir da engine;
+        # ver https://docs.sqlalchemy.org/en/14/core/connections.html
+        # #working-with-the-dbapi-cursor-directly
+        conector_dbapi = conexao.raw_connection()  # type: ignore
 
-#     with conector_dbapi.cursor() as cursor:  # type: ignore
-#         buffer = StringIO()
-#         writer = csv.writer(buffer)
-#         writer.writerows(dados_iterador)
-#         buffer.seek(0)
+    with conector_dbapi.cursor() as cursor:  # type: ignore
+        buffer = StringIO()
+        writer = csv.writer(buffer)
+        writer.writerows(dados_iterador)
+        buffer.seek(0)
 
-#         enumeracao_colunas = ", ".join(
-#             '"{}"'.format(coluna) for coluna in colunas
-#         )
-#         if tabela_dados.schema:
-#             tabela_nome = "{}.{}".format(
-#                 tabela_dados.schema,
-#                 tabela_dados.name,
-#             )
-#         else:
-#             tabela_nome = tabela_dados.name
+        enumeracao_colunas = ", ".join(
+            '"{}"'.format(coluna) for coluna in colunas
+        )
+        if tabela_dados.schema:
+            tabela_nome = "{}.{}".format(
+                tabela_dados.schema,
+                tabela_dados.name,
+            )
+        else:
+            tabela_nome = tabela_dados.name
 
-#         expressao_sql = "COPY {} ({}) FROM STDIN WITH CSV".format(
-#             tabela_nome,
-#             enumeracao_colunas,
-#         )
-#         cursor.copy_expert(sql=expressao_sql, file=buffer)  # type: ignore
+        expressao_sql = "COPY {} ({}) FROM STDIN WITH CSV".format(
+            tabela_nome,
+            enumeracao_colunas,
+        )
+        cursor.copy_expert(sql=expressao_sql, file=buffer)  # type: ignore
 
-#     return None
+    return None
 
 
-# def carregar_dataframe(
-#     sessao: Session,
-#     df: pd.DataFrame,
-#     tabela_destino: str,
-#     passo: int | None = 10000,
-#     teste: bool = False,
-# ) -> int:
-#     """Carrega dados públicos para o banco de dados analítico da ImpulsoGov.
+def carregar_dataframe(
+    sessao: Session,
+    df: pd.DataFrame,
+    tabela_destino: str,
+    passo: int | None = 10000,
+    teste: bool = False,
+) -> int:
+    """Carrega dados públicos para o banco de dados analítico da ImpulsoGov.
 
-#     Argumentos:
-#         sessao: objeto [`sqlalchemy.orm.session.Session`][] que permite
-#             acessar a base de dados da ImpulsoGov.
-#         df: [`DataFrame`][] contendo os dados a serem carregados na tabela de
-#             destino, já no formato utilizado pelo banco de dados da ImpulsoGov.
-#         tabela_destino: nome da tabela de destino, qualificado com o nome do
-#             schema (formato `nome_do_schema.nome_da_tabela`).
-#         passo: Indica quantos registros devem ser enviados para a base de dados
-#             de cada vez. Por padrão, são inseridos 10.000 registros em cada
-#             transação. Se o valor for `None`, todo o DataFrame é carregado de
-#             uma vez.
-#         teste: Indica se o carregamento deve ser executado em modo teste. Se
-#             verdadeiro, faz *rollback* de todas as operações; se falso, libera
-#             o ponto de recuperação criado.
+    Argumentos:
+        sessao: objeto [`sqlalchemy.orm.session.Session`][] que permite
+            acessar a base de dados da ImpulsoGov.
+        df: [`DataFrame`][] contendo os dados a serem carregados na tabela de
+            destino, já no formato utilizado pelo banco de dados da ImpulsoGov.
+        tabela_destino: nome da tabela de destino, qualificado com o nome do
+            schema (formato `nome_do_schema.nome_da_tabela`).
+        passo: Indica quantos registros devem ser enviados para a base de dados
+            de cada vez. Por padrão, são inseridos 10.000 registros em cada
+            transação. Se o valor for `None`, todo o DataFrame é carregado de
+            uma vez.
+        teste: Indica se o carregamento deve ser executado em modo teste. Se
+            verdadeiro, faz *rollback* de todas as operações; se falso, libera
+            o ponto de recuperação criado.
 
-#     Retorna:
-#         Código de saída do processo de carregamento. Se o carregamento
-#         for bem sucedido, o código de saída será `0`.
+    Retorna:
+        Código de saída do processo de carregamento. Se o carregamento
+        for bem sucedido, o código de saída será `0`.
 
-#     Note:
-#         Esta função não faz *commit* das alterações do banco. Após o retorno
-#         desta função, o commit deve ser feito manualmente (método
-#         `sessao.commit()`) ou implicitamente por meio do término sem erros de
-#         um gerenciador de contexto (`with Sessao() as sessao: # ...`) no qual a
-#         função de carregamento tenha sido chamada.
+    Note:
+        Esta função não faz *commit* das alterações do banco. Após o retorno
+        desta função, o commit deve ser feito manualmente (método
+        `sessao.commit()`) ou implicitamente por meio do término sem erros de
+        um gerenciador de contexto (`with Sessao() as sessao: # ...`) no qual a
+        função de carregamento tenha sido chamada.
 
-#     [`sqlalchemy.orm.session.Session`]: https://docs.sqlalchemy.org/en/14/orm/session_api.html#sqlalchemy.orm.Session
-#     [`DataFrame`]: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html
-#     [`transformar_pa()`]: impulsoetl.siasus.procedimentos.transformar_pa
-#     """
+    [`sqlalchemy.orm.session.Session`]: https://docs.sqlalchemy.org/en/14/orm/session_api.html#sqlalchemy.orm.Session
+    [`DataFrame`]: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html
+    [`transformar_pa()`]: impulsoetl.siasus.procedimentos.transformar_pa
+    """
 
-#     num_registros = len(df)
-#     schema_nome, tabela_nome = tabela_destino.split(".", maxsplit=1)
+    num_registros = len(df)
+    schema_nome, tabela_nome = tabela_destino.split(".", maxsplit=1)
 
-#     logging.info(
-#         "Carregando {num_registros} registros de procedimentos ambulatoriais "
-#         "para a tabela `{tabela_destino}`...",
-#         num_registros=num_registros,
-#         tabela_destino=tabela_destino,
-#     )
+    logging.info(
+        "Carregando {num_registros} registros de procedimentos ambulatoriais "
+        "para a tabela `{tabela_destino}`...",
+        num_registros=num_registros,
+        tabela_destino=tabela_destino,
+    )
 
-#     logging.debug("Formatando colunas de data...")
-#     colunas_data = df.select_dtypes(include="datetime").columns
-#     df[colunas_data] = df[colunas_data].applymap(
-#         lambda dt: dt.isoformat() if pd.notna(dt) else None
-#     )
+    logging.debug("Formatando colunas de data...")
+    colunas_data = df.select_dtypes(include="datetime").columns
+    df[colunas_data] = df[colunas_data].applymap(
+        lambda dt: dt.isoformat() if pd.notna(dt) else None
+    )
 
-#     logging.info("Copiando registros...")
+    logging.info("Copiando registros...")
 
-#     ponto_de_recuperacao = sessao.begin_nested()
-#     conexao = sessao.connection()
-#     try:
-#         df.to_sql(
-#             name=tabela_nome,
-#             con=conexao,
-#             schema=schema_nome,
-#             if_exists="append",
-#             index=False,
-#             chunksize=passo,
-#             method=postgresql_copiar_dados,
-#         )
-#     # trata exceções levantadas pelo backend
-#     except (DBAPIError, Psycopg2Error) as erro:
-#         ponto_de_recuperacao.rollback()
-#         sessao.rollback()
-#         if isinstance(erro, DBAPIError):
-#             erro.hide_parameters = True
-#             erro = cast(Psycopg2Error, erro.orig)
-#         logging.error(
-#             "Erro ao inserir registros na tabela `{}` (Código {})",
-#             tabela_destino,
-#             erro.pgcode,
-#         )
-#         logging.debug(
-#             "({}.{}) {}",
-#             erro.__class__.__module__,
-#             erro.__class__.__name__,
-#             erro.pgerror,
-#         )
-#         return erro.pgcode
-#     else:
-#         ponto_de_recuperacao.commit()
+    ponto_de_recuperacao = sessao.begin_nested()
+    conexao = sessao.connection()
+    try:
+        df.to_sql(
+            name=tabela_nome,
+            con=conexao,
+            schema=schema_nome,
+            if_exists="append",
+            index=False,
+            chunksize=passo,
+            method=postgresql_copiar_dados,
+        )
+    # trata exceções levantadas pelo backend
+    except (DBAPIError, Psycopg2Error) as erro:
+        ponto_de_recuperacao.rollback()
+        sessao.rollback()
+        if isinstance(erro, DBAPIError):
+            erro.hide_parameters = True
+            erro = cast(Psycopg2Error, erro.orig)
+        logging.error(
+            "Erro ao inserir registros na tabela `{}` (Código {})",
+            tabela_destino,
+            erro.pgcode,
+        )
+        logging.debug(
+            "({}.{}) {}",
+            erro.__class__.__module__,
+            erro.__class__.__name__,
+            erro.pgerror,
+        )
+        return erro.pgcode
+    else:
+        ponto_de_recuperacao.commit()
 
-#     logging.info("Carregamento concluído.")
+    logging.info("Carregamento concluído.")
 
-#     return 0
+    return 0
